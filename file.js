61737d7864986baae6afef5946a4719702898079
SUANTI KATALINA BERNAL ORTIZ <suantiortiz@gmail.com>
2025-03-15 12:23:34 -0300

    calculadora de descuento

--- file.js
+++ file.js
@@ -3,4 +3,8 @@
 
 // EXERCISE 1
 // Escribe un programa que calcule el precio final de un producto despues de aplicar un descuento
-// Pide al usuario que ingrese el precio original del producto y el porcentaje de descuento y muestre el precio final 
+// Pide al usuario que ingrese el precio original del producto y el porcentaje de descuento y muestre el precio final
+llet precio= 
+let descuento= 
+let PrecioFinal= precio*(descuento/100)
+console.log(PrecioFinal)
