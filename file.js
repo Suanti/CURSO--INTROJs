fad0a715c50573bf8673e3b890f5475df102a649
SUANTI KATALINA BERNAL ORTIZ <suantiortiz@gmail.com>
2025-03-15 12:09:41 -0300

    inicio ejercicio 1

--- file.js
+++ file.js
@@ -1,2 +1,6 @@
-// intento de modificar archivo
+// EXERCISE
 // segundo intento 
+
+// EXERCISE 1
+// Escribe un programa que calcule el precio final de un producto despues de aplicar un descuento
+// Pide al usuario que ingrese el precio original del producto y el porcentaje de descuento y muestre el precio final 
