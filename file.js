f19a74ab013c17fb96f86a825378ea4a01fc65e8
SUANTI KATALINA BERNAL ORTIZ <suantiortiz@gmail.com>
2025-03-15 12:42:22 -0300

    calculadora de descuento con ventanillas emergentes

--- file.js
+++ file.js
@@ -4,7 +4,12 @@
 // EXERCISE 1
 // Escribe un programa que calcule el precio final de un producto despues de aplicar un descuento
 // Pide al usuario que ingrese el precio original del producto y el porcentaje de descuento y muestre el precio final
-llet precio= 
+let precio= 
 let descuento= 
 let PrecioFinal= precio*(descuento/100)
 console.log(PrecioFinal)
+//nueva version 
+let precio= parseFloat(prompt('ingresa el precio original'))
+let descuento= parseFloat(prompt('ingresa el valor del descuento'))
+let PrecioFinal= precio*(descuento/100)
+console.log('precio finnal:' ,PrecioFinal)
