c17702b42a8798471741eeb5dba310ac55ee4916
SUANTI KATALINA BERNAL ORTIZ <suantiortiz@gmail.com>
2025-03-18 11:01:20 -0300

    EJERCICIO 7 - variable de asignación temporaria

--- file.js
+++ file.js
@@ -228,7 +228,12 @@ b = a - b; // b ahora es 5 (valor original de a)
 a = a - b; // a ahora es 10 (valor original de b)
 console.log("Después del intercambio: a =", a, ", b =", b);
 
-
+// VARIABLE ASIGNACIÓN SIMULTÁNEA (intercambio en una línea)
+let a = 5;
+let b = 10;
+console.log("Antes del intercambio: a =", a, ", b =", b);
+[a, b] = [b, a];
+console.log("Después del intercambio: a =", a, ", b =", b);
 
 /*
 ●	Ejercicio 8 - Conversión de Temperaturas: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit. La fórmula de conversión es: 
